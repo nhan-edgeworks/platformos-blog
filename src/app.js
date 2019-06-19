8ae97d5b638160d859803c2805e2f69db20ec856
nhan.pham <nhan.pham@edge-works.net>
2019-06-19 14:29:06 +0700

    add webpack to theme build

--- src/app.js
+++ src/app.js
@@ -1,5 +1,2 @@
 import 'bootstrap';
 import './app.scss';
-
-
-// import CustomAttributesDependentVisibility from './scripts/CustomAttributesDependentVisibility';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const theme_path = './modules/dashboard_blog/public/views/partials/themes/';
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    app: './src/app.js',
+    'themes/blog2': theme_path + 'blog2/assets/theme.js',
+  },
   output: {
-    filename: 'app.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'marketplace_builder/assets')
   },
   module: {
@@ -21,5 +25,8 @@ module.exports = {
           'css-loader'
         ]
     },]
+  },
+  node: {
+    fs: "empty"
   }
 };
